518ce31faacc401f6e3197d903634b8fcfdf2d19
stcygbrlp <stacygabrielep@gmail.com>
2025-01-02 14:37:40 +0800

    added back buttons

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -24,6 +24,15 @@
         </div>
     </nav>
 
+    <!-- Back Button -->
+    <div class="flex justify-start mt-8 px-6">
+        <a href="http://mhdef162ils.test">
+            <button class="bg-black text-white py-2 px-6 rounded-lg hover:bg-gray-800 transition duration-300">
+                Back
+            </button>
+        </a>
+    </div>
+
     <!-- Main Content -->
     <main class="container mx-auto px-6 py-12 text-center mt-0">
         <h2 class="text-4xl font-bold mb-8">Patron Dashboard</h2>
--- resources/views/user/room/index.blade.php
+++ resources/views/user/room/index.blade.php
@@ -26,7 +26,7 @@
 
     <!-- Back Button -->
     <div class="flex justify-start mt-8 px-6">
-        <a href="{{ route('librarian.index') }}">
+        <a href="{{ route('reservations.usercreate') }}">
             <button class="bg-black text-white py-2 px-6 rounded-lg hover:bg-gray-800 transition duration-300">
                 Back
             </button>
