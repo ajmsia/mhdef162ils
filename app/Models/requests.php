1541a03af8216b15c2a5a6623a13015d8172db80
vadlargo <vdlargo@up.edu.ph>
2025-01-05 12:06:52 +0800

    added primary key definition for querying the show function

--- app/Models/requests.php
+++ app/Models/requests.php
@@ -9,6 +9,7 @@ class requests extends Model
 {
     use SoftDeletes;
 
+    protected $primaryKey = 'requestID'; // laravel expects that the default column name for our primary key is 'id'. but since i changed it to 'requestID', it should be able to recognize the requestID primary key!
     protected $fillable = [
         'requestID', 'userFirstName', 'userLastName', 'userMiddleName', 'upmail', 'college', 'userType', 'title', 'resourceType', 'tuklasLink', 'requestDate'
     ];
